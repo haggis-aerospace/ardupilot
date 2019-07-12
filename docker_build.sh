d47dd1a3abaf602f08d65b5ff819b80ca03a2b39
HaggisAerospaceService <haggisaerospace@dundee.ac.uk>
2019-07-12 07:59:49 +0000

    test of pushing images

--- docker_build.sh
+++ docker_build.sh
@@ -3,6 +3,8 @@ board="CubeBlack"
 commit=$(git rev-parse --short HEAD)
 date=$(date +%F)
 
+pip install future
+
 echo "Starting Build Process"
 #git clone https://github.com/haggis-aerospace/ardupilot ardupilot_docker
 #cd ardupilot_docker
@@ -12,7 +14,7 @@ make $board
 
 mkdir -p Release/$board/$date/$commit
 echo "Build Complete"
-cp build/$board/bin/* Release/$board/$date/$commit/
+cp build/$board/bin Release/$board/$date/$commit
 echo "Cleaning"
 make clean
 exit
